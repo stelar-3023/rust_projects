60af031e53853e49cae59168f5109774b2587354
Steve Larsen <slarsen-3@att.net>
2023-05-26 18:50:06 -0500

    guessing game - part 1

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io; // bring io library into scope
+
+fn main() { // main function
+    println!("Guess the number!"); // print line
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // create a mutable variable
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
