663e3defef081ac199a5cd51652bfbe364e0c29a
Steve Larsen <slarsen-3@att.net>
2023-05-27 18:05:53 -0500

    added rand to prject to generate random number between 1-100 inclusive

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
+use rand::Rng; // bring Rng trait into scope
 use std::io; // bring io library into scope
 
-fn main() { // main function
+fn main() {
+    // main function
     println!("Guess the number!"); // print line
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // generate random number
+
+    println!("The secret number is: {secret_number}",); // print secret number
 
     println!("Please input your guess.");
 
